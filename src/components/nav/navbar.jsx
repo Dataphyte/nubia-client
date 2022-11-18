11f735d6f11b89a41c557a21bde1623ce90eeb6a
Emmanuel Alawode <alawodeemmanuel2@gmail.com>
2022-11-18 15:11:28 +0100

    fix: added new features to the nav

--- src/components/nav/navbar.jsx
+++ src/components/nav/navbar.jsx
@@ -1,7 +1,9 @@
 'use client';
 
-import React from 'react';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
+import React, { useState } from 'react';
+import MenuBars from '@/icons/menu-bars';
 import { usePathname } from 'next/navigation';
 import useScrolled from '@/hooks,useScrolled';
 import { classNames } from 'src/utils/classnames';
@@ -15,11 +17,16 @@ const navItems = [
 const Navbar = () => {
   const pathname = usePathname();
   const scrolled = useScrolled();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenu = () => {
+    setMenuOpen((state) => !state);
+  };
 
   return (
     <nav
       className={classNames(
-        'w-full flex px-5 md:px-16 items-center justify-between fixed top-0 z-50 duration-300 ease-out transition-all',
+        'w-full flex px-10 md:px-16 items-center justify-between fixed top-0 z-50 duration-300 ease-out transition-all',
         scrolled ? 'bg-white-main shadow-md lg:px-32 h-16' : ' h-20 lg:px-48'
       )}
     >
@@ -28,13 +35,13 @@ const Navbar = () => {
       </Link>
 
       {/* ====== NAV LINKS */}
-      <span className='flex  px-4 gap-8'>
+      <span className='hidden md:flex  px-4 gap-8'>
         {navItems.map((item, index) => (
           <Link
             href={item.href}
             key={index}
             className={classNames(
-              'nav-link',
+              'nav__link',
               pathname.indexOf(item.href) !== -1 && 'text-violet-main'
             )}
           >
@@ -46,10 +53,67 @@ const Navbar = () => {
       {/* ====== CALL TO ACTION */}
       <Link
         href='/'
-        className='py-2 px-5 text-sm rounded-lg shadow font-inter bg-violet-dark text-white-off duration-150 ease-out hover:shadow-lg'
+        className='hidden md:block py-2 px-5 text-sm rounded-lg shadow font-inter bg-violet-dark text-white-off duration-150 ease-out hover:shadow-lg'
       >
         Add a story
       </Link>
+
+      {/* ====== MOBILE MENU */}
+      <span
+        className='md:hidden w-14 h-12 p-2 border border-violet-main cursor-pointer rounded-md shadow-lg'
+        onClick={handleMenu}
+      >
+        <MenuBars sx='w-10 h-8' />
+      </span>
+
+      <div
+        className={classNames(
+          'md:hidden h-screen flex flex-col items-center pt-72 absolute top-0 right-0 bg-[#1E293B] duration-300 ease-out',
+          menuOpen ? 'w-full right-0 opacity-1' : 'w-0 -right-10 opacity-0'
+        )}
+      >
+        {/* ====== NAV LINKS */}
+        <motion.span
+          initial={{ opacity: 0, width: 0, border: 'none' }}
+          whileInView={{
+            opacity: 1,
+            width: '80%',
+            borderTop: 'thin solid gray',
+          }}
+          transition={{ delay: 0.3, duration: 0.6 }}
+          className='flex h-full flex-col items-center justify-center px-4 gap-28 border-t border-white-off/80 pt-28 md:hidden whitespace-nowrap overflow-hidden relative'
+        >
+          {navItems.map((item, index) => (
+            <motion.span
+              initial={{ opacity: 0, position: 'absolute', top: 0 }}
+              whileInView={{
+                opacity: 1,
+                position: 'absolute',
+                top: `${(index + 1) * 120}px`,
+              }}
+              transition={{
+                delay: (index + 1) * 0.4,
+                duration: 0.15,
+                type: 'spring',
+                damping: 20,
+                stiffness: 700,
+              }}
+              className='flex w-max h-max'
+            >
+              <Link
+                href={item.href}
+                key={index}
+                className={classNames(
+                  'nav__link-mobile',
+                  pathname.indexOf(item.href) !== -1 && 'text-violet-main'
+                )}
+              >
+                {item.label}
+              </Link>
+            </motion.span>
+          ))}
+        </motion.span>
+      </div>
     </nav>
   );
 };

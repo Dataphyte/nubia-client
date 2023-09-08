a2be2aa066af942f111cd1a21179040302be1197
Emmanuel Alawode <alawodeemmanuel2@gmail.com>
2023-09-08 22:10:03 +0100

    feat: signup page started

--- src/assets/icons/cursor-arrow-rays.jsx
+++ src/assets/icons/cursor-arrow-rays.jsx
@@ -5,7 +5,7 @@ const CursorArrowRays = ({ fill, sx }) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      fill={'none' | fill}
+      fill={'none' || fill}
       viewBox='0 0 24 24'
       strokeWidth='1.5'
       stroke='currentColor'

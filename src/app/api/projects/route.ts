9cd495bee4336490063cb61ec70849706802dd95
Emmanuel Alawode <alawodeemmanuel2@gmail.com>
2023-09-12 22:15:05 +0100

    feat: fixed some bugs

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,5 +1,5 @@
-import { , NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-  return NextResponse.json({ messgae: 'No new projects here ' });
+  return NextResponse.json({ messgae: 'No new projects here' });
 }

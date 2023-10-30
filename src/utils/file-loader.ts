07d6f58cd71f11271080070348d8605a0fec4ef6
Emmanuel Alawode <alawodeemmanuel2@gmail.com>
2023-10-30 18:23:55 +0000

    fix: passed Editor constructor instead of instance

--- src/utils/file-loader.ts
+++ src/utils/file-loader.ts
@@ -24,21 +24,18 @@ export const fileLoader = ({
     setState &&
       contents &&
       setState({
-        parsed: Papa.parse<string | LocalFile | ArrayBuffer | undefined>(
-          contents,
-          {
-            header: true,
-            complete: () => {
-              setNotificationContent({
-                text: 'Parsing Complete',
-                type: 'success',
-                description:
-                  'Your data has been parsed. You may continue to preview or customize your features.',
-              });
-              setShow(true);
-            },
-          }
-        ),
+        parsed: Papa.parse<string | ArrayBuffer>(contents, {
+          header: true,
+          complete: () => {
+            setNotificationContent({
+              text: 'Parsing Complete',
+              type: 'success',
+              description:
+                'Your data has been parsed. You may continue to preview or customize your features.',
+            });
+            setShow(true);
+          },
+        }),
         contents,
         features: [],
         details,

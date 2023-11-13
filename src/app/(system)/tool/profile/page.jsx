123ab8031de5546c76d926333b6c56e513c18af9
Emmanuel Alawode <alawodeemmanuel2@gmail.com>
2023-11-13 16:07:49 +0000

    chore: added user edit form to profile page

--- src/app/(system)/tool/profile/page.jsx
+++ src/app/(system)/tool/profile/page.jsx
@@ -40,8 +40,116 @@ const Profile = () => {
       </div>
 
       {/* -- section 2 */}
-      <section className='grid grid-cols-5 w-full lg:px-6 gap-3 lg:-mt-20'>
-        <div className='col-span-5 lg:col-span-3 h-auto  bg-white-main shadow-lg rounded-lg'></div>
+      <section className='grid grid-cols-5 w-full mb-10 lg:px-6 gap-3 lg:-mt-20'>
+        <div className='col-span-5 lg:col-span-3 h-auto px-2 lg:px-4 py-5 bg-white-main shadow-lg rounded-lg'>
+          <div className='border-b border-gray-900/10 pb-12'>
+            <h2 className='text-base font-semibold leading-7 text-gray-900 font-bold'>
+              Account Information
+            </h2>
+
+            <div className='mt-5 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6'>
+              <div className='sm:col-span-3'>
+                <label
+                  htmlFor='name'
+                  className='block text-sm font-medium leading-6 text-gray-900'
+                >
+                  Name
+                </label>
+                <div className='mt-2'>
+                  <input
+                    type='text'
+                    name='name'
+                    id='name'
+                    value={user?.name}
+                    autoComplete='name'
+                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  />
+                </div>
+              </div>
+
+              <div className='sm:col-span-3'>
+                <label
+                  htmlFor='last-name'
+                  className='block text-sm font-medium leading-6 text-gray-900'
+                >
+                  Email Address
+                </label>
+                <div className='mt-2'>
+                  <input
+                    type='email'
+                    name='email'
+                    id='email'
+                    value={user?.email}
+                    autoComplete='email'
+                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  />
+                </div>
+              </div>
+
+              <div className='sm:col-span-3'>
+                <label
+                  htmlFor='country'
+                  className='block text-sm font-medium leading-6 text-gray-900'
+                >
+                  Account type
+                </label>
+                <div className='mt-2'>
+                  <select
+                    id='country'
+                    name='country'
+                    autoComplete='country-name'
+                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6'
+                  >
+                    <option value='individual'>Individual</option>
+                    <option value='organization'>Organization</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className='col-span-full'>
+                <label
+                  htmlFor='street-address'
+                  className='block text-sm font-medium leading-6 text-gray-900'
+                >
+                  Old password
+                </label>
+                <div className='mt-2'>
+                  <input
+                    type='password'
+                    name='street-address'
+                    id='street-address'
+                    autoComplete='street-address'
+                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  />
+                </div>
+              </div>
+
+              <div className='col-span-full'>
+                <label
+                  htmlFor='street-address'
+                  className='block text-sm font-medium leading-6 text-gray-900'
+                >
+                  New Password
+                </label>
+                <div className='mt-2'>
+                  <input
+                    type='password'
+                    name='street-address'
+                    id='street-address'
+                    autoComplete='street-address'
+                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='col-span-6 mt-4 bg-ornage-500'>
+            <button className='py-2 px-6 rounded-md shadow transition-all duration-300 ease-out text-white-off bg-green-main hover:shadow-lg'>
+              Save changes
+            </button>
+          </div>
+        </div>
+
         <div className='col-span-5 lg:col-span-2 h-max bg-white-main shadow-lg rounded-lg flex flex-col items-center py-10 gap-4 px-2'>
           {/* -- Userv profile image   */}
           <div
